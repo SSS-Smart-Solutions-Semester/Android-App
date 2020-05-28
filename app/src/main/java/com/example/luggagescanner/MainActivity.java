00b13faeb20655db4faa37cc06c0f598324976ea
Yaolee <yaoleeteoh@gmail.com>
2020-05-28 17:37:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/luggagescanner/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.luggagescanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.DefaultCompany.HelloARU3D.UnityPlayerActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UnityPlayerActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
