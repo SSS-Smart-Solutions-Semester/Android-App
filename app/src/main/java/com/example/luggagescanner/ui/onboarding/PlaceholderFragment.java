7478697bbc3d0f77e77ea80ad022f52f0acda997
Yaolee <yaoleeteoh@gmail.com>
2020-06-10 16:09:53 +0200

    Finished onboarding screens

--- app/src/main/java/com/example/luggagescanner/ui/onboarding/PlaceholderFragment.java
+++ app/src/main/java/com/example/luggagescanner/ui/onboarding/PlaceholderFragment.java
@@ -1,9 +1,11 @@
 package com.example.luggagescanner.ui.onboarding;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -22,6 +24,7 @@ public class PlaceholderFragment extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
 
     private PageViewModel pageViewModel;
+    private Context mCtx;
 
     public static PlaceholderFragment newInstance(int index) {
         PlaceholderFragment fragment = new PlaceholderFragment();
@@ -40,6 +43,7 @@ public class PlaceholderFragment extends Fragment {
             index = getArguments().getInt(ARG_SECTION_NUMBER);
         }
         pageViewModel.setIndex(index);
+        mCtx = this.getContext();
     }
 
     @Override
@@ -48,15 +52,23 @@ public class PlaceholderFragment extends Fragment {
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_onboarding, container, false);
 
-        // TODO show steps based on the pager index we're in
-        final TextView textView = root.findViewById(R.id.section_label);
-        pageViewModel.getText().observe(this, new Observer<String>() {
+        final ImageView ivOnboarding = root.findViewById(R.id.iv_onboarding);
+        final TextView tvHeader = root.findViewById(R.id.tv_onboarding_header);
+        final TextView tvBody = root.findViewById(R.id.tv_onboarding_body);
+
+        pageViewModel.getIndex().observe(this, new Observer<Integer>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onChanged(Integer index) {
+                ivOnboarding.setImageResource(getResourceId("ic_illustrations_all_0" + index, "drawable"));
+                tvHeader.setText(getResourceId("header_" + index, "string"));
+                tvBody.setText(getResourceId("body_" + index, "string"));
             }
-        });
+        });;
 
         return root;
+    }
+
+    private int getResourceId(String id, String defType) {
+        return getResources().getIdentifier(id, defType, mCtx.getPackageName());
     }
 }
