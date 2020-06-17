0b73bb6058878dd2fca77862e758316f1b240a6d
Yaolee <yaoleeteoh@gmail.com>
2020-06-17 23:44:19 +0200

    Dynamic bottom sheet

--- app/src/main/java/com/example/luggagescanner/ui/MainActivity.java
+++ app/src/main/java/com/example/luggagescanner/ui/MainActivity.java
@@ -3,14 +3,12 @@ package com.example.luggagescanner.ui;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.DefaultCompany.HelloARU3D.UnityPlayerActivity;
 import com.example.luggagescanner.databinding.ActivityMainBinding;
@@ -18,7 +16,7 @@ import com.example.luggagescanner.ui.onboarding.Onboarding;
 import com.example.luggagescanner.R;
 import com.example.luggagescanner.utils.SharedPrefs;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavController.OnDestinationChangedListener {
     public static final String SCAN_CARD_TRANSITION_NAME = "scan_card_";
     public static final String SHOW_ONBOARDING = "show_onboarding";
     public static final int ONBOARDING_CODE = 123;
@@ -38,10 +36,9 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.bottomSheet.button.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, UnityPlayerActivity.class);
-            startActivity(intent);
-        });
+        setBottomSheetForHome();
+
+        Navigation.findNavController(this, R.id.nav_host_fragment).addOnDestinationChangedListener(this);
     }
 
     @Override
@@ -54,5 +51,32 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, UnityPlayerActivity.class);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+        switch (destination.getId()) {
+            case R.id.homeFragment:
+                setBottomSheetForHome();
+                break;
+            case R.id.scanFragment:
+                setBottomSheetForScan();
+                break;
+        }
+    }
+
+    private void setBottomSheetForHome() {
+        binding.bottomSheet.button.setText(R.string.button_ar);
+        binding.bottomSheet.button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, UnityPlayerActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void setBottomSheetForScan() {
+        binding.bottomSheet.button.setText(R.string.button_close);
+        binding.bottomSheet.button.setOnClickListener(v -> {
+            Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp();
+        });
     }
 }
